f61d91ea06889ca0d3200cb1cf75b1cd69342016
victor <vinh.ntuan@gmail.com>
2024-04-19 17:33:25 +0200

    Added UI to rock paper scissors

--- odin-rock-paper-scissors/game.js
+++ odin-rock-paper-scissors/game.js
@@ -1,46 +1,67 @@
-const moves = ["Rock", "Paper", "Scissors"];
+const moves = ["rock", "paper", "scissors"];
+let player_options = document.getElementById("player-options");
+let buttons = player_options.querySelectorAll("#player-options > button");
+let round_result_div = document.getElementById("round-result");
+let human_score_div = document.getElementById("human-score");
+let computer_score_div = document.getElementById("computer-score");
+let computer_select_div = document.getElementById("computer-selection")
+let human_score = 0;
+let computer_score = 0;
 
-game();
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log(button.id);
+        play_round(button.id);
+    })
+});
 
-function game(){
-    let humanScore = 0;
-    for (let i = 0; i < 5; i++ ){
-        let playerSelection = humanPlay();
-        let computerSelection = computerPlay();
 
-        let result = playRound(playerSelection, computerSelection);
-        console.log(`Round ${i}: ` + result);
-        if (result.includes("won")) humanScore++;
-        if (result.includes("drew")) humanScore += 0.5;
-    }
-    
-    console.log(`Game ended. You got ${humanScore} points!`);
 
-    if (humanScore == 2.5)
-        console.log("DRAW :|")
-    else if (humanScore > 2.5)
-        console.log("You won buddy!!! :)");
-    else 
-        console.log("Nice try pissbaby :(");      
+function play_round(player_selection){
+    let computer_selection = computerPlay();
+    computer_select_div.innerText = `Computer played ${computer_selection}`;
+    let result = get_result(player_selection, computer_selection);
+    update(result);
+    display_winner();
 }
 
-
-function playRound(playerSelection, computerSelection){
-    let playerIndex = moves.indexOf(playerSelection);
-    let compIndex = moves.indexOf(computerSelection);
+function get_result(player_selection, computer_selection){
+    let playerIndex = moves.indexOf(player_selection);
+    let compIndex = moves.indexOf(computer_selection);
     let indexDiff = playerIndex - compIndex;
     if (indexDiff < 0) indexDiff += 3;
 
-    if (indexDiff === 0) {
-        return `You drew!!! Both threw ${playerSelection}`;
-    } else if (indexDiff === 1) {
-        return `You won!!! ${playerSelection} beats ${computerSelection}`;
+    return indexDiff;
+}
+
+function update(round_result){
+    if (round_result === 0) {
+        round_result_div.innerText = "You drew";
+    } else if (round_result === 1) {
+        round_result_div.innerText = "You won !";
+        human_score += 1;
     } else {
-        return `You lost!!! ${playerSelection} loses to ${computerSelection}`;
+        round_result_div.innerText = "You lost Bozo";
+        computer_score += 1;
+    }
+    computer_score_div.innerText = `Computer score is: ${computer_score}`;
+    human_score_div.innerText = `Your score is: ${human_score}`;
+}
+
+function display_winner(){
+    if (human_score === 5) {
+        reset();
+        alert("Human won !");
+    } else if (computer_score === 5) {
+        reset();
+        alert("Computer won ! Blehh");
     }
 }
 
-
+function reset(){
+    human_score = 0;
+    computer_score = 0;
+}
 
 
 function computerPlay(){
@@ -48,21 +69,6 @@ function computerPlay(){
     return moves[ranInt];
 }
 
-function humanPlay(){
-    while (true) {
-        let move = prompt("What doya wanna play?");
-        if(!move) {
-            alert("No move selected");
-            continue;
-        }
-    
-        move = capitalise(move.toLowerCase());
-        if (moves.includes(move)) 
-            return move;
-        else 
-            alert("Invalid move");      
-    }
-}
 
 function randomInt(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
